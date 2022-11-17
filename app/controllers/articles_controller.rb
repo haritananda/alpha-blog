6ea64a2af6d0e9baf08d8e79ec4279e1308fa54d
Harita Nanda <haritananda@gmail.com>
2022-11-17 14:22:58 +1100

    Added bootstrap and Added User Table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,54 @@
 class ArticlesController < ApplicationController
+
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
         #debugger
-        @article = Article.find(params[:id])
     end
+
     def index
         @articles = Article.all
-    
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        #debugger
+    end
+
+    def create
+        @article = Article.new(article_params) #whitelist and makes the title & description available
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to(@article)
+        else
+            render "new", status: :unprocessable_entity
+        end
+      end      
+    
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render "edit", status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private 
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
   root 'pages#home' #Rails knows it is application_controller
   get 'about',to:'pages#about' #Get the about page and then mention where to send this request
-  resources :articles, only: [:show, :index]
+  resources :articles # ,only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
