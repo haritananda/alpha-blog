3b5a76e525c8ce9c4f1d7382f1d40df1e1378a44
Harita Nanda <haritananda@gmail.com>
2022-11-19 20:30:39 +1100

    add users create action

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   root 'pages#home' #Rails knows it is application_controller
   get 'about',to:'pages#about' #Get the about page and then mention where to send this request
   resources :articles # ,only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
